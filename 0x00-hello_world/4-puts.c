d1437a3e693f90f300f04c6a7ed896420920356a
B-Akapo <bunmi.i.akapo@gmail.com>
2022-09-08 17:16:06 -0700

    edited 4-puts.c to fit betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,13 @@
  * main - print the string in the put function
  * Description: using the main function
  * this program prints Programming is like building a multilingual puzzle
- * Return: 0
+ * Return 0
  */
 
 
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 
