133c6968192c94cce968f55572a87e74ac7daa49
B-Akapo <bunmi.i.akapo@gmail.com>
2022-09-13 17:06:38 -0700

    created 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints '_putchar'
+ * Return: Always 0 (success)
+*/
+
+int main()
+{
+	char print[] = "_putchar";
+	putchar(print);
+	putchar(\n);
+	return 0;
+
+}
