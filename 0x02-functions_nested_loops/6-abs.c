288d5cc10825b468a5b94a876ea7e6c58999b801
B-Akapo <bunmi.i.akapo@gmail.com>
2022-09-14 22:47:03 +0100

    created 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _abs - returns the absolute value of a number given to it
+* @num: a given value
+* Return: return 0 if successful 1 if otherwise
+*/
+
+int _abs(int num)
+{
+	if (num > 0)
+	{
+		return (num);
+	}
+	else if (n < 0)
+	{
+		return (-(num));
+	}
+	else
+	{
+		return (num);
+	}
+}
